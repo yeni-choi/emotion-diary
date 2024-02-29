1e409d21b70c77402d94f2a9aea759b4597c0284
yeni-choi <yewonchoi@ewhain.net>
2024-03-01 01:10:40 +0900

    init: create pages

---
+++ src/pages/Diary.js
@@ -0,0 +1,10 @@
+const Diary = () => {
+    return (
+        <div>
+            <h1>Diary</h1>
+            <p>This is Diary Detail Page.</p>
+        </div>
+    );
+};
+
+export default Diary;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>This is Home.</p>
+        </div>
+    );
+};
+
+export default Home;
